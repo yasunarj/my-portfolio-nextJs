7a6e14f2e2e7f23dda3032d1f031aa7e8109aeac
nari <yasunarj@gmail.com>
2025-01-06 19:31:37 +0900

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col max-h-screen`}
       >
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const Home = () => {
   return (
-    <div className="h-screen-vh">
-      <div className="flex flex-col items-center justify-center space-y-8 h-[80%]">
+    <div className="h-screen flex flex-col"> 
+      <Header />
+      <div className="flex-grow flex flex-col items-center justify-center space-y-8 h-[80%]">
         <h1 className="text-4xl font-bold text-gray-700 tracking-wider">
           🚀Next Game🚀
         </h1>
@@ -20,6 +23,7 @@ const Home = () => {
           </Button>
         </Link>
       </div>
+      <Footer />
     </div>
   );
 };
